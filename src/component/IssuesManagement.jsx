a5027d97da62b8d8e832c60349639064d0a8412b
alriad56 <r39717473@gmail.com>
2025-10-17 22:32:08 +0600

    new commit

--- src/component/IssuesManagement.jsx
+++ src/component/IssuesManagement.jsx
@@ -1,22 +1,51 @@
-import React, { use,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from './Container';
 import Toggles_btns from './Toggle_btns';
 import Countbox from './Countbox';
-const IssuesManagement = ({fetchPromise}) => {
-    const [toggleStatus,setToggleStatus] = useState("All")
-    const initialData = use(fetchPromise)
-    console.log(initialData)
-    return (
+
+const IssuesManagement = ({ fetchPromise }) => {
+  const [toggleStatus, setToggleStatus] = useState("All");
+  const [initialData, setInitialData] = useState([]);
+
+  useEffect(() => {
+    // Wait for the fetchPromise to resolve
+    fetchPromise.then((data) => {
+      setInitialData(data);
+    });
+  }, [fetchPromise]);
+
+  console.log(initialData);
+
+  return (
+    <div>
+      <Countbox />
+
+      {/* Toggle buttons */}
+      <Toggles_btns toggleStatus={toggleStatus} setToggleStatus={setToggleStatus} />
+
+      {/* Cards */}
+      {initialData.map((issue) => {
+        console.log(issue);
+       return (
+       <div className='shadow-md rounded-md cursor pointer bg-state-100'>
         <div>
-  <Countbox/>
-  {/*Toggle buttons*/}
-    <Toggles_btns toggleStatus={toggleStatus} setToggleStatus={setToggleStatus} />
-
-    {/*cards*/}
-
-    
+        <img src={issue.image} alt="" 
+        className='h-[80px] w-[80px] rounded-full'
+        />
+        <h2 className='font-semibold text-lg'>{issue.requestedBy}</h2>
         </div>
-    );
+       <div className='flex justify-between'>
+       <h2 className='font-bold text-[26px]'>{issue.subject}</h2>
+        <span>{issue.priority}</span>
+        <span>{issue.status}</span>
+       </div>
+        <p className='text-[17px] text-slate-700'>{issue.description}</p>
+       </div>
+
+       );
+      })}
+    </div>
+  );
 };
 
 export default IssuesManagement;
